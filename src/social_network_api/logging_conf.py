3a6918d95ee8bb7973832d1ce68724da1db0f258
jean.dossantos <jeandsantos88@gmail.com>
2023-12-03 21:54:33 +0100

    Added logging configuration

---
+++ src/social_network_api/logging_conf.py
@@ -0,0 +1,59 @@
+from logging.config import dictConfig
+
+from social_network_api.config import DevConfig, config
+
+
+def configure_logging() -> None:
+    dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "console": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(name)s:%(lineno)d - %(message)s",
+                },
+                "file": {
+                    "class": "logging.Formatter",
+                    "datefmt": "%Y-%m-%dT%H:%M:%S",
+                    "format": "%(asctime)s.%(msecs)03dZ | %(levelname)-8s | %(name)s:%(lineno)d - %(message)s",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "class": "rich.logging.RichHandler",
+                    "level": "DEBUG",
+                    "formatter": "console",
+                },
+                "rotating_file": {
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "level": "DEBUG",
+                    "formatter": "file",
+                    "filename": "logs.log",
+                    "maxBytes": 1024 * 1024,
+                    "backupCount": 5,
+                    "encoding": "utf8",
+                },
+            },
+            "loggers": {
+                "uvicorn": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "INFO",
+                },
+                "databases": {
+                    "handlers": ["default"],
+                    "level": "WARNING",
+                },
+                "aiosqlite": {
+                    "handlers": ["default"],
+                    "level": "WARNING",
+                },
+                "src": {
+                    "handlers": ["default", "rotating_file"],
+                    "level": "DEBUG" if isinstance(config, DevConfig) else "INFO",
+                    "propagate": False,
+                },
+            },
+        }
+    )
--- src/social_network_api/main.py
+++ src/social_network_api/main.py
@@ -1,14 +1,19 @@
+import logging
 from contextlib import asynccontextmanager
-from typing import Union
 
 from fastapi import FastAPI
 
 from social_network_api.database import database
+from social_network_api.logging_conf import configure_logging
 from social_network_api.routers import post
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
+    logger.info("Connecting database")
     await database.connect()
     yield
     await database.disconnect()
